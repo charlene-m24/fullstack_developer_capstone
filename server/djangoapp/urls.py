a23fae61a166fc499c5c151ded76704156daf3b1
charlene-m24 <gmqiti@gmail.com>
2025-06-30 09:08:54 -0400

    logout function- module 2

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -6,7 +6,8 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
+    path(route='login', view=views.login_user, name='login'),
     path(route='get_cars', view=views.get_cars, name ='getcars'),
-
+    path(route='logout', view=views.logout_request, name="logout"),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
